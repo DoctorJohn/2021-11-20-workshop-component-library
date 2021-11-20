d4ca026b71d970f506f5374cd0ca9cc964dfc22d
Jonathan Ehwald <github@ehwald.info>
2021-11-20 16:52:42 +0100

    Implement the contact details page

--- src/pages/Application/steps/ApplicationPageContactDetails/ApplicationPageContactDetails.tsx
+++ src/pages/Application/steps/ApplicationPageContactDetails/ApplicationPageContactDetails.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { ApplicationPageStep } from "../../components/ApplicationPageStep";
+import {
+  InputField,
+  InputType,
+} from "../../../../components/InputField/InputField";
+import { Select } from "../../../../components/Select/Select";
+import "./ApplicationPageContactDetails.css";
 
 interface ApplicationPageContactDetailsProps {
   handleGoToNextStep: (pageData: unknown) => void;
@@ -21,7 +27,55 @@ export const ApplicationPageContactDetails = ({
       showPreviousPageButton={false}
       handleGoToPreviousStep={handleGoToPreviousStep}
     >
-      <div></div>
+      <div>
+        <div className="side-by-side">
+          <InputField
+            placeholderText="Erika"
+            type={InputType.text}
+            label="Vorname"
+            validation={true}
+            validationText=""
+          />
+          <InputField
+            placeholderText="Mustermann"
+            type={InputType.text}
+            label="Nachname"
+            validation={true}
+            validationText=""
+          />
+        </div>
+        <br/>
+        <InputField
+          placeholderText="erika.mustermann@email.de"
+          type={InputType.text}
+          label="E-Mail"
+          validation={true}
+          validationText=""
+        />
+        <br/>
+        <InputField
+          placeholderText="+49 123 2222 2222"
+          type={InputType.text}
+          label="Handy"
+          validation={true}
+          validationText=""
+        />
+        <br/>
+        <Select
+          label="Staatsangehoerigkeit"
+          options={["Deutsch", "Nicht-Deutsch"]}
+          placeholder="Staatsangehoerigkeit"
+        />
+        <br/>
+        <InputField
+          placeholderText="Ipfelerstrasse 15, 68789, St.Leon-Rot"
+          type={InputType.text}
+          label="Adresse"
+          validation={true}
+          validationText=""
+        />
+        <br/>
+      </div>
     </ApplicationPageStep>
   );
 };
